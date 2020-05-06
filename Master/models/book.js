dfef4b743bc8cf14d0e04b27d57fbb8c555512fe
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-05-06 14:15:15 -0500

    models are done

--- Master/models/book.js
+++ Master/models/book.js
@@ -1,16 +1,29 @@
+// require dependencies
+  // mongoose npm 
 const mongoose = require("mongoose");
+  // Schema is a template from mongoose that is used to create the models
 const Schema = mongoose.Schema;
 
+// define BookSchema as a new Schema (or a new template) with the following key pairs
 const bookSchema = new Schema({
+  // title will be a required string
   title: { type: String, required: true },
+  // subtitle will be a string
   subtitle: { type: String },
+  // authors will be an array of strings- it is a required field
   authors: { type: [String], required: true },
+  // link will be a required string
   link: { type: String, required: true },
+  // description will be a required string
   description: { type: String, required: true },
+  // image will be a required string
   image: { type: String, required: true },
+  // image will be a required string- it also must be unique
   googleId: { type: String, required: true, unique: true }
 });
 
+// define const called Book which will reference mongoose's model method using the Book model and bookSchema
 const Book = mongoose.model("Book", bookSchema);
 
+// export as Book to use in index.js in the models folder
 module.exports = Book;
--- Master/models/index.js
+++ Master/models/index.js
@@ -1,3 +1,5 @@
+// require the Book model from the book.js file
+  // all models are compiled into index.js in the models folder so that the controllers only need to look for this particular index.js file
 module.exports = {
   Book: require("./book")
 };
