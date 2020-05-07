718df5c1691a396e7925743f7769a50c931132a4
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-05-07 11:37:48 -0500

    card

--- Master/client/src/components/Card/index.js
+++ Master/client/src/components/Card/index.js
@@ -4,19 +4,22 @@ import React from "react";
 // functional component called Card
 // passing in icon, title, and children
 function Card({ icon, title, children }) {
-  // returning a Bootstrap card
+  // returning a Bootstrap card layout
   return (
     <div className="card mt-4">
       <div className="card-header">
         <h3>
           <strong>
+            {/* displays icon/title passed in through props*/}
             <i className={`fa fa-${icon}`} aria-hidden="true" /> {title}
           </strong>
         </h3>
       </div>
+      {/* passing the element body as children as element body has yet to be determined*/}
       <div className="card-body">{children}</div>
     </div>
   );
 }
 
+// export out as Card
 export default Card;
