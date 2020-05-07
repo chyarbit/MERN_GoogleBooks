b70f3b1cfccd391953aa242b9c0191db5cf13dcb
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-05-07 12:38:00 -0500

    completed saved

--- Master/client/src/pages/Saved.js
+++ Master/client/src/pages/Saved.js
@@ -1,3 +1,4 @@
+// require dependencies
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import Card from "../components/Card";
@@ -7,34 +8,47 @@ import API from "../utils/API";
 import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
 
+// class component called Saved which extends the Component class in React
 class Saved extends Component {
+  // set initial set
   state = {
     books: []
   };
 
+  // method componentDidMount will run method getSavedBooks when everything is loaded and ready
   componentDidMount() {
     this.getSavedBooks();
   }
 
+  // method getSavedBooks to get all saved books
   getSavedBooks = () => {
+    // call API to get getSavedBooks method
     API.getSavedBooks()
+      // when successful, will set the state for key books with response data
       .then(res =>
         this.setState({
           books: res.data
         })
       )
+      // if unsuccessful, will log an error
       .catch(err => console.log(err));
   };
 
+  // method handleBookDelete to delete a specific book basd on the book id
   handleBookDelete = id => {
-    API.deleteBook(id).then(res => this.getSavedBooks());
+    // call API to get deleteBook method and delete book with the specific id that was passed in
+    API.deleteBook(id)
+    // after deletion, will run getSavedBooks to get the updated list of saved books
+    .then(res => this.getSavedBooks());
   };
 
+  // render 
   render() {
     return (
       <Container>
         <Row>
           <Col size="md-12">
+            {/* use Jumbotron component*/}
             <Jumbotron>
               <h1 className="text-center">
                 <strong>(React) Google Books Search</strong>
@@ -45,10 +59,13 @@ class Saved extends Component {
         </Row>
         <Row>
           <Col size="md-12">
+            {/* use Card component*/}
             <Card title="Saved Books" icon="download">
               {this.state.books.length ? (
+                {/* use List component*/}
                 <List>
                   {this.state.books.map(book => (
+                    {/* use Book component*/}
                     <Book
                       key={book._id}
                       title={book.title}
@@ -80,4 +97,5 @@ class Saved extends Component {
   }
 }
 
+// export as Saved
 export default Saved;
