54c69ded4c4a12367c8d5af86b3941ae175f21e7
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-05-07 12:33:12 -0500

    nomatch done

--- Master/client/src/pages/NoMatch.js
+++ Master/client/src/pages/NoMatch.js
@@ -1,12 +1,15 @@
+// require dependencies 
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 
+// function called NoMatch will return an error if no match for the book can be found
 function NoMatch() {
   return (
     <Container fluid>
       <Row>
         <Col size="md-12">
+          {/* use Jumbotron component*/}
           <Jumbotron>
             <h1 className="text-center">404 Page Not Found</h1>
             <h1 className="text-center">
@@ -21,4 +24,5 @@ function NoMatch() {
   );
 }
 
+// export as NoMatch
 export default NoMatch;
